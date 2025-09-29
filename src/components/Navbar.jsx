e07c36d5c525e9e26556e1e8f574a32fea0a807a
FGEGitHub <pipao.pipo@gmail.com>
2025-09-29 17:46:09 -0300

    as

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">Club Atlético Quilmes</div>
-      <ul className="nav-links">
-        <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/calendario">Calendario</Link></li>
+
+      {/* Botón Hamburguesa */}
+      <div className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </div>
+
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setIsOpen(false)}>Inicio</Link></li>
+        <li><Link to="/calendario" onClick={() => setIsOpen(false)}>Calendario</Link></li>
         <li className="dropdown">
           <span>Deportes ▾</span>
           <ul className="dropdown-menu">
-            <li><Link to="/deportes/futbol-masculino">Fútbol Masculino</Link></li>
-            <li><Link to="/deportes/futbol-femenino">Fútbol Femenino</Link></li>
-            <li><Link to="/deportes/basquet">Básquet</Link></li>
-            <li><Link to="/deportes/hockey">Hockey</Link></li>
+            <li><Link to="/deportes/futbol-masculino" onClick={() => setIsOpen(false)}>Fútbol Masculino</Link></li>
+            <li><Link to="/deportes/futbol-femenino" onClick={() => setIsOpen(false)}>Fútbol Femenino</Link></li>
+            <li><Link to="/deportes/basquet" onClick={() => setIsOpen(false)}>Básquet</Link></li>
+            <li><Link to="/deportes/hockey" onClick={() => setIsOpen(false)}>Hockey</Link></li>
           </ul>
         </li>
-        <li><Link to="/quienes-somos">Quiénes Somos</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
+        <li><Link to="/quienes-somos" onClick={() => setIsOpen(false)}>Quiénes Somos</Link></li>
+        <li><Link to="/contacto" onClick={() => setIsOpen(false)}>Contacto</Link></li>
       </ul>
     </nav>
   );
